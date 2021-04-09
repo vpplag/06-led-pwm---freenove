d60cf58ba4dfb7537c1fbb2e10bbc47d836bf7e7
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-09 19:36:48 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
-let state = 0
-let counter = 0
+let button_pressed = 0
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
         // Debounce
         basic.pause(10)
         if (pins.digitalReadPin(DigitalPin.P0) == 0) {
-            state = 0
+            button_pressed = 1
         }
     }
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         // Debounce
         basic.pause(10)
         if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-            state = 1
+            button_pressed = 0
         }
     }
 })
 basic.forever(function () {
-    for (let counter = 0; counter <= 500; counter++) {
-        if (state) {
+    if (button_pressed) {
+        for (let counter = 0; counter <= 500; counter++) {
             pins.analogWritePin(AnalogPin.P1, counter)
             basic.pause(1)
+            if (!(button_pressed)) {
+                pins.analogWritePin(AnalogPin.P1, 0)
+                break;
+            }
         }
-    }
-    for (let counter = 0; counter <= 500; counter++) {
-        if (state) {
+        for (let counter = 0; counter <= 500; counter++) {
             pins.analogWritePin(AnalogPin.P1, 500 - counter)
             basic.pause(1)
+            if (!(button_pressed)) {
+                pins.analogWritePin(AnalogPin.P1, 0)
+                break;
+            }
         }
     }
 })
